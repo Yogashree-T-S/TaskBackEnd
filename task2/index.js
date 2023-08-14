1f06e86b157370ce919c7638e4368b8d7b27835d
Yogashree <yogashreesekar@gmail.com>
2023-08-14 09:39:27 +0530

    first commit

---
+++ task2/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+let app= express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());
+app.post('/index', function(req, res){
+    let name=req.body.name.trim();
+    let age=(req.body.age+"").trim();
+    if(name!=='' && age!==''){
+        for(let val of name){
+            if(val.toUpperCase()!==val.toLowerCase())
+                continue;
+            res.status(400).send("Don't include numbers in name")
+        }
+        for(let val of age){
+            if(val.toUpperCase()===val.toLowerCase())
+                continue;
+            res.status(400).send("Don't include alphabet in age")
+        }
+        res.status(200).send("valid")       
+    }
+    res.status(400).send('Invalid')
+    
+    
+})
+app.listen(8080,function(){
+    console.log('listening on');
+})
